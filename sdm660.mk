8f31249fb235ab6342d098ce6aa87a51ef3d3ddf
ben863 <r.budhiono@gmail.com>
2022-08-16 04:51:26 +0700

    sdm660-common: BringUp AOSP base
    Change-Id: I04314d1eef4812706de56124173d0f3547b30461
    Signed-off-by: ben863 <r.budhiono@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -305,7 +305,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-arrow
+    $(LOCAL_PATH)/overlay-aosp
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
